f1aae0b1c08a3c6776d423520208006c72815587
belema7 <belemagirma31@gmail.com>
2026-02-27 15:45:07 +0300

    fix route webhooks secretket

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -4,7 +4,7 @@ import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
 import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req) {
-  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+  const SIGNING_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
   if (!SIGNING_SECRET) {
     throw new Error(
